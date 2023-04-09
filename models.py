0fb379b38542dcfdd7be509329ce414ba25647ac
rahatmb <89215260+rahatmb@users.noreply.github.com>
2023-04-09 22:16:27 +0600

    Add files via upload

--- models.py
+++ models.py
@@ -1,27 +1,12 @@
-import email
-from logging import exception
+from venv import create
 from django.db import models
-from django.contrib.auth.models import User
-from base.models import BaseModel
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 import uuid
-from base.emails import send_account_activation_email
 
-class Profile(BaseModel):
-    user = models.OneToOneField(User , on_delete=models.CASCADE ,related_name="profile")
-    is_email_verified = models.BooleanField(default=False)
-    email_token = models.CharField(max_length=100 , null=True ,blank=True)
-    profile_image = models.ImageField(upload_to = "profile")
-    
-@receiver(post_save , sender = User)
-def send_email_token(sender , instance , created , **kwargs):
-    try:
-        if created:
-            email_token = str(uuid.uuid4())
-            Profile.objects.create(user = instance , email_token = email_token)
-            email = instance.email
-            send_account_activation_email(email , email_token)
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True,editable=False , default=uuid.uuid4)
+    create_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
-    except Exception as e :
-        print(e)       
+    class Meta:
+        abstract = True
+
